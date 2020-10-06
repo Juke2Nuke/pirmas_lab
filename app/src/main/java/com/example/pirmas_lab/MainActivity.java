d2341dd56f5057115d052ab1679e6135542ffa2c
Juke2Nuke <julius.pliaugo@gmail.com>
2020-10-06 23:23:25 +0300

    change button and textfield names

--- app/src/main/java/com/example/pirmas_lab/MainActivity.java
+++ app/src/main/java/com/example/pirmas_lab/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.pirmas_lab;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView tvMain;
+    private Button button01;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tvMain = findViewById(R.id.tvMain);
+        this.button01 = findViewById(R.id.button01);
+    }
+
+    public void btnChangeText(View view) {
+        this.tvMain.setText("Funnie");
+        this.button01.setText("HaHa");
     }
 }
